83bdc19c867a50b9063f00460f55a08513f739eb
polatengin <enpolat@microsoft.com>
2023-11-25 22:11:16 +0000

    configuring identity core to accept passwords that has certain chars

--- src/option2/Program.cs
+++ src/option2/Program.cs
@@ -21,6 +21,19 @@ builder.Services.AddAuthentication().AddBearerToken(IdentityConstants.BearerSche
   options.BearerTokenExpiration = TimeSpan.FromDays(365);
 });
 
+builder.Services.Configure<IdentityOptions>(options =>
+{
+  options.Password.RequireDigit = true;
+  options.Password.RequireLowercase = true;
+  options.Password.RequireNonAlphanumeric = true;
+  options.Password.RequireUppercase = true;
+  options.Password.RequiredLength = 6;
+  options.Password.RequiredUniqueChars = 1;
+
+  options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+  options.User.RequireUniqueEmail = true;
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
