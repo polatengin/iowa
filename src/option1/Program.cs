a1784ae2a911ecb962ce638a9beac252f726a564
polatengin <enpolat@microsoft.com>
2023-11-25 21:53:46 +0000

    creating database context to save user info

--- src/option1/Program.cs
+++ src/option1/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -7,3 +11,10 @@ var app = builder.Build();
 app.MapControllers();
 
 app.Run();
+
+public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+{
+  public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+  {
+  }
+}
